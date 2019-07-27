4dc2b9eb3266a0b3e6eded8852a13ed5a2e2e4a7
davr59 <davr59@outlook.com>
2019-07-26 21:53:25 -0600

    add properties optional parameter to showDoorbellFeedback

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-var application = require("application");
-var platform = require("platform");
+var application = require('application');
+var platform = require('platform');
 
 var exports = {};
 
 if (platform.isAndroid) {
-  exports.showDoorbellFeedback = function(appId, appKey) {
-    const feedback = new io.doorbell.android.Doorbell(application.android.startActivity, parseInt(appId), appKey);
+  exports.showDoorbellFeedback = function(appId, appKey, properties = null) {
+    const feedback = new io.doorbell.android.Doorbell(
+      application.android.startActivity,
+      parseInt(appId),
+      appKey
+    );
+    if (properties) {
+      for (var p in properties) {
+        feedback.addProperty(p, properties[p]);
+      }
+    }
     feedback.show();
-  }
+  };
 } else {
-  exports.showDoorbellFeedback = function(appId, appKey, viewController, animated = true) {
+  exports.showDoorbellFeedback = function(
+    appId,
+    appKey,
+    viewController,
+    properties = null,
+    animated = true
+  ) {
     const feedback = Doorbell.alloc().initWithApiKeyAppId(appKey, appId);
     feedback.animated = animated;
-    feedback.showFeedbackDialogInViewControllerCompletion(viewController, (error) => {});
-  }
+    if (properties) {
+      for (var p in properties) {
+        feedback.addPropertyWithNameAndValue(p, properties[p]);
+      }
+    }
+    feedback.showFeedbackDialogInViewControllerCompletion(viewController, error => {});
+  };
 }
 
 module.exports = exports;
