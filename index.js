afbf917f10810d7a1d30964a70c6f40295c7e4db
davr59 <davr59@outlook.com>
2020-02-09 22:34:12 -0600

    fix android function signature so is similar to ios

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ if (platform.isAndroid) {
   exports.showDoorbellFeedback = function(
     appId,
     appKey,
+    viewController,
     properties = undefined
   ) {
     const feedback = new io.doorbell.android.Doorbell(
